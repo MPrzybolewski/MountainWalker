e827a5c085462379ad573a099ceac17746b2ce24
Marek Przybolewski <mprzybolewski@gmail.com>
2017-12-30 17:55:49 +0100

    add to MenuViewModel Achievements, Trials and Description View Models

--- MountainWalker.Core/ViewModels/MenuViewModel.cs
+++ MountainWalker.Core/ViewModels/MenuViewModel.cs
@@ -22,6 +22,36 @@ namespace MountainWalker.Core.ViewModels
             _navigationService.Navigate<HomeViewModel>();
         }
 
+        public IMvxCommand ShowTrialCommand
+        {
+            get { return new MvxCommand(ShowTrialExecuted); }
+        }
+
+        private void ShowTrialExecuted()
+        {
+            _navigationService.Navigate<TrialsViewModel>();
+        }
+
+        public IMvxCommand ShowAchievementsCommand
+        {
+            get { return new MvxCommand(ShowAchievementsExecuted); }
+        }
+
+        private void ShowAchievementsExecuted()
+        {
+            _navigationService.Navigate<AchievementsViewModel>();
+        }
+
+        public IMvxCommand ShowAppDescriptionCommand
+        {
+            get { return new MvxCommand(ShowAppDescriptionExecuted); }
+        }
+
+        private void ShowAppDescriptionExecuted()
+        {
+            _navigationService.Navigate<AppDescriptionViewModel>();
+        }
+
         public IMvxCommand ShowSettingCommand
         {
             get { return new MvxCommand(ShowSettingsExecuted); }
