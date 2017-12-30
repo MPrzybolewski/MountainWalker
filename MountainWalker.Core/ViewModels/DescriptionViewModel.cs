d1233e9519b7053491679af459153121ff5c66d5
Marek Przybolewski <mprzybolewski@gmail.com>
2017-12-30 17:42:17 +0100

    add AchievemntsViewModel, DescriptionViewModel and TrialsViewModel

---
+++ MountainWalker.Core/ViewModels/DescriptionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace MountainWalker.Core.ViewModels
+{
+    public class DescriptionViewModel : MvxViewModel
+    {
+        public DescriptionViewModel()
+        {
+        }
+    }
+}
---
+++ MountainWalker.Core/ViewModels/TrialsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace MountainWalker.Core.ViewModels
+{
+    public class TrialsViewModel : MvxViewModel
+    {
+        public TrialsViewModel()
+        {
+        }
+    }
+}
