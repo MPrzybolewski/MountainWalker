ebcde547742b5a8dabd48e131800a8dfe83e2372
Marek Przybolewski <mprzybolewski@gmail.com>
2018-01-05 18:52:58 +0100

    add IBottomPanelService

---
+++ MountainWalker.Core/Interfaces/IBottomPanelService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MountainWalker.Core.Models;
+
+namespace MountainWalker.Core.Interfaces
+{
+    public interface IBottomPanelService
+    {
+        void GetTimeFromTimer();
+        void SetTravelTime(TravelTime travelTime);
+        void SetNumberOfReachedPoints(int numberOfReachedPoints);
+        void StartTimer();
+        void StopTimer();
+        void SetTravelTime();
+        TravelTime GetTravelTime();
+    }
+}
