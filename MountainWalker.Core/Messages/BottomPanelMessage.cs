cd543cc663f12abdc3276953b7864938e4980e0d
Marek Przybolewski <mprzybolewski@gmail.com>
2018-01-05 18:37:45 +0100

    add fields to BottomPanelMessage

--- MountainWalker.Core/Messages/BottomPanelMessage.cs
+++ MountainWalker.Core/Messages/BottomPanelMessage.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using MountainWalker.Core.Models;
 using MvvmCross.Plugins.Messenger;
 
 namespace MountainWalker.Core.Messages
 {
     public class BottomPanelMessage : MvxMessage
     {
-        public BottomPanelMessage(object sender) : base(sender)
+        public TravelTime TravelTime 
         {
+            get;
+            private set;
         }
+
+        public int NumberOfReachedPoints
+        {
+            get;
+            private set;
+        }
+        public BottomPanelMessage(object sender, TravelTime travelTime, int numberOfReachedPoints) : base(sender)
+        {
+            TravelTime = travelTime;
+            NumberOfReachedPoints = numberOfReachedPoints;
+        }
+
+
     }
 }
