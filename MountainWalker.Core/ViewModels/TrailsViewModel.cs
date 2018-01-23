f34ee4607d382e568772f29d3521f0a54081d461
Marek Przybolewski <mprzybolewski@gmail.com>
2018-01-23 17:14:35 +0100

    add Trails List

--- MountainWalker.Core/ViewModels/TrailsViewModel.cs
+++ MountainWalker.Core/ViewModels/TrailsViewModel.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using MountainWalker.Core.Interfaces;
+using MountainWalker.Core.Models;
+using MountainWalker.Core.Services;
+using MvvmCross.Core.Navigation;
 using MvvmCross.Core.ViewModels;
 
 namespace MountainWalker.Core.ViewModels
 {
     public class TrailsViewModel : MvxViewModel
     {
-        public TrailsViewModel()
+        
+        private readonly ITrailService _trailService;
+        private readonly IMvxNavigationService _navigationService;
+        private readonly ILocationService _locationService;
+
+        public TrailsViewModel(ITrailService trailService, IMvxNavigationService navigationService, ILocationService locationService)
         {
+            _trailService = trailService;
+            _navigationService = navigationService;
+            _locationService = locationService;
+
+            Items = _trailService.Trails;
         }
+
+        private List<Trail> _items;
+
+        public List<Trail> Items
+        {
+            get { return _items; }
+            set { _items = value; RaisePropertyChanged(() => Items); }
+        }
+
+        public ICommand ShowDetailTrail
+        {
+            get
+            {
+                return new MvxCommand<Trail>(item =>
+                {
+                    _locationService.TrailId = item.Id;
+                    Debug.WriteLine("TEST TEST TEST {0}", item.Id);
+                    _navigationService.Navigate<TrailDetailsViewModel>();
+                });
+            }
+        }
+
     }
 }
