4e6419ba02aebe2fb07db262d8d718ad6733fae0
Marek Przybolewski <mprzybolewski@gmail.com>
2018-05-25 14:11:29 +0200

    add reached travels for ios

---
+++ MountainWalker.Touch/Views/ReachedTrailCell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Foundation;
+using MountainWalker.Core.Models;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using UIKit;
+
+namespace MountainWalker.Touch.Views
+{
+	public partial class ReachedTrailCell : MvxTableViewCell
+    {
+        public static readonly NSString Key = new NSString("ReachedTrailCell");
+        public static readonly UINib Nib;
+
+        static ReachedTrailCell()
+        {
+            Nib = UINib.FromName("ReachedTrailCell", NSBundle.MainBundle);
+        }
+
+		protected ReachedTrailCell(IntPtr handle) : base(handle)
+		{
+			this.DelayBind(() =>
+			{
+				var set = this.CreateBindingSet<ReachedTrailCell, ReachedTrail>();
+				set.Bind(DateTextCell).To(vm => vm.Date);
+				set.Bind(DescriptionTextCell).To(vm => vm.Desc);
+				set.Apply();
+			});
+		}
+    }
+}
